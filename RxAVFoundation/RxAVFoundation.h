16898de060512374e74663f53621f81196f947b3
junhyeong hong <hjh5696@snu.ac.kr>
2020-11-16 15:43:11 +0100

    support carthage

---
+++ RxAVFoundation/RxAVFoundation.h
@@ -0,0 +1,18 @@
+//
+//  RxAVFoundation.h
+//  RxAVFoundation
+//
+//  Created by JunHyeong Hong on 2020/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxAVFoundation.
+FOUNDATION_EXPORT double RxAVFoundationVersionNumber;
+
+//! Project version string for RxAVFoundation.
+FOUNDATION_EXPORT const unsigned char RxAVFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAVFoundation/PublicHeader.h>
+
+
